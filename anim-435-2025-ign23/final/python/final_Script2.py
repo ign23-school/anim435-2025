05526d13da610e5778db6305b4485b2952a1c5c9
ign23-school <ign23@drexel.edu>
2025-12-11 13:39:06 -0500

    Wrote out first draft of script

---
+++ anim-435-2025-ign23/final/python/final_Script2.py
@@ -0,0 +1,31 @@
+import os
+import csv
+import maya.cmds 
+
+shot = os.getenv("SHOT")
+
+csv_path =   # Wherever cvs gets saved to!
+
+slate = ""
+f = open(csv_path, "r")
+reader = csv.DictReader(f)
+for row in reader:
+    if row["shot"] == shot:
+        slate = row["slate"]
+f.close()
+
+camera_name = "CAM_" + slate
+cam, cam_shape = cmds.camera()
+cmds.rename(cam, camera_name)
+
+user = os.getenv("USERNAME")
+export_folder = f"C:/Users/{user}/FinalExportedCam"
+os.makedirs(export_folder)
+
+output_path = export_folder + "/" + "CAM_" + shot + ".fbx"
+
+cmds.select(camera_name)
+cmds.file(output_path, type="FBX export", exportSelected=True)
+
+print("Created camera:", camera_name)
+print("Exported FBX to:", output_path)
