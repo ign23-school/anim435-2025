ddc0c6110d77da2eef67c9d38abfb81874e9753d
ign23-school <ign23@drexel.edu>
2025-12-12 11:45:10 -0500

    Adding Comments to script

--- anim-435-2025-ign23/final/python/final_Script2.py
+++ anim-435-2025-ign23/final/python/final_Script2.py
@@ -2,10 +2,10 @@ import os
 import csv
 import maya.cmds 
 
-shot = os.getenv("SHOT")
+shot = os.getenv("SHOT")         # Gets environmental variables from current Gitbash shell
 csv_path = os.getenv("CSV_FILE")
 
-slate = ""
+slate = ""                       # Gets slate name by reading it from the CSV
 f = open(csv_path, "r")
 reader = csv.DictReader(f)
 
@@ -15,15 +15,15 @@ for row in reader:
 
 f.close()
 
-camera_name = "CAM_" + slate
+camera_name = "CAM_" + slate     # Gets camera name from slate in the CSV
 
 user = os.getenv("USERNAME")
-export_folder = f"C:/Users/{user}/FinalExportedCam"
+export_folder = f"C:/Users/{user}/FinalExportedCam"       # Builds folder to place the exported camera in
 os.makedirs(export_folder)
 
-output_path = export_folder + "/CAM_" + shot + ".fbx"
+output_path = export_folder + "/CAM_" + shot + ".fbx"     # Builds the filename for exported camera
 
 cmds.select(camera_name)
-cmds.file(output_path, type="FBX export", exportSelected=True)
+cmds.file(output_path, type="FBX export", exportSelected=True)    # Selects camera and exports as an fbx with the specified name to the specified path
 
-print("Exported FBX to:", output_path)
+print("Exported FBX to:", output_path)  # Prints to confirm export 
