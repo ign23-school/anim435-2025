767f075c3e801aa7c184dec371b44d3f68ddfb45
ign23-school <ign23@drexel.edu>
2025-12-11 21:37:14 -0500

    Fixing errors

--- anim-435-2025-ign23/final/python/final_Script2.py
+++ anim-435-2025-ign23/final/python/final_Script2.py
@@ -8,17 +8,22 @@ csv_path = os.getenv("CSV_FILE")
 slate = ""
 f = open(csv_path, "r")
 reader = csv.DictReader(f)
+
 for row in reader:
     if row["shot"] == shot:
         slate = row["slate"]
+
 f.close()
 
 camera_name = "CAM_" + slate
 
-if not cmds.objExists(camera_name):
-    print("ERROR: Camera does not exist:", camera_name)
-    return
-
 user = os.getenv("USERNAME")
 export_folder = f"C:/Users/{user}/FinalExportedCam"
-os.makedirs(export_folder, exist_ok=
+os.makedirs(export_folder)
+
+output_path = export_folder + "/CAM_" + shot + ".fbx"
+
+cmds.select(camera_name)
+cmds.file(output_path, type="FBX export", exportSelected=True)
+
+print("Exported FBX to:", output_path)
